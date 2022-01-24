303a15aebbf2686d2c32c1e0a596054049149cae
jaanus <jaanus.alnek@vikk.ee>
2022-01-24 16:48:16 +0200

    added database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 
-const server = http.createServer(()=>{
+const server = http.createServer((req,res)=>{
     res.statuscode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello!');
