70ae229db6765cf0524f93fa295625fa5faa4476
Jaanus <jaanus@stagnationlab.com>
2022-02-03 10:19:31 +0200

    changed ormconfig behaviour

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,19 +1,10 @@
 import { createConnection, ConnectionOptions } from 'typeorm';
-import config from '../config/config.json';
 
 export async function openDatabaseConnection() {
   // await closeDatabaseConnection();
 
-  console.log({...config});
+  const conn = await createConnection();
 
-  const conn = await createConnection({
-      type: "mysql",
-      host: config.host,
-      port: config.port,
-      database: "blog",
-      username: config.username,
-      password: config.password,
-  });
   if (!conn.isConnected) {
     throw new Error('Connection to database failed');
   }
