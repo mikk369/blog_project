4397153fa23a6a05e7e05807f545666dbb66e393
mikk369 <mikk.ereline@vikk.ee>
2022-05-04 21:51:22 +0300

    deleteCategory

---
+++ src/routes/categories/deleteCategory.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express';
+import Post from '../../entities/Category';
+const router = express.Router();
+
+//find post by id
+router.delete('/:id', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const deleted = await Post.delete({ id: id });
+
+    if (!deleted) {
+      return res.json({
+        message: 'no category found with ID: ' + id
+      });
+    }
+    console.log(deleted);
+    return res.status(200).json(deleted);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(404).json({
+        error: 'Unable to find category',
+        message: error.message
+      });
+    }
+    // unknown (typeorm error?)
+    return res.status(400).json({
+      error: 'Unable to delete',
+      message: 'unknown error'
+    });
+  }
+});
+
+export default router;
