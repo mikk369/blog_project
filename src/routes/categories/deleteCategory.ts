c6c4e036452e8435b376ca34d5f73c4ad720a6c8
mikk369 <mikk.ereline@vikk.ee>
2022-05-08 18:02:54 +0300

    works

--- src/routes/categories/deleteCategory.ts
+++ src/routes/categories/deleteCategory.ts
@@ -1,30 +1,30 @@
 import express, { Request, Response } from 'express';
-import Post from '../../entities/Category';
+import Category from '../../entities/Category';
 const router = express.Router();
 
-//find post by id
+//find category by id
 router.delete('/:id', async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
 
-    const deleted = await Post.delete({ id: id });
+    const deleted = await Category.delete({ id: id });
 
     if (!deleted) {
-      return res.json({
-        message: 'no category found with ID: ' + id
+      return res.status(404).json({
+        message: 'no category found with given ID: ' + id
       });
     }
-    console.log(deleted);
+
     return res.status(200).json(deleted);
   } catch (error) {
     if (error instanceof Error) {
-      return res.status(404).json({
+      return res.json({
         error: 'Unable to find category',
         message: error.message
       });
     }
     // unknown (typeorm error?)
-    return res.status(400).json({
+    return res.json({
       error: 'Unable to delete',
       message: 'unknown error'
     });
